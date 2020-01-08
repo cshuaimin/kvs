1548791d574c83361f467ae83d1a7d7a2105ca76
Chen Shuaimin <chen_shuaimin@outlook.com>
2020-01-08 17:17:30 +0800

    Put benchmarks for KvStore and Sled into groups correctly

--- benches/benches.rs
+++ benches/benches.rs
@@ -19,44 +19,44 @@ pub fn benchmark(c: &mut Criterion) {
     }
 
     macro_rules! bench_write {
-        ($engine:ident) => {
-            let mut group = c.benchmark_group("write");
+        ($engine:ident, $group:expr) => {
             let temp_dir = TempDir::new().unwrap();
             let mut store = $engine::open(temp_dir.path()).unwrap();
-            group.bench_function(BenchmarkId::new(stringify!($engine), ""), |b| {
+            $group.bench_function(BenchmarkId::new(stringify!($engine), ""), |b| {
                 b.iter_batched(
                     || (get_key!(), get_value!()),
                     |(key, value)| store.set(key, value).unwrap(),
                     BatchSize::SmallInput,
                 )
             });
-            group.finish();
         };
     }
 
     macro_rules! bench_read {
-        ($engine:ident) => {
-            let mut group = c.benchmark_group("read");
+        ($engine:ident, $group:expr) => {
             let temp_dir = TempDir::new().unwrap();
             let mut store = $engine::open(temp_dir.path()).unwrap();
             (0..100).for_each(|_| {
                 store.set(get_key!(), get_value!()).unwrap();
             });
-            group.bench_function(BenchmarkId::new(stringify!($engine), ""), |b| {
+            $group.bench_function(BenchmarkId::new(stringify!($engine), ""), |b| {
                 b.iter_batched(
                     || get_key!(),
                     |key| store.get(key).unwrap(),
                     BatchSize::SmallInput,
                 )
             });
-            group.finish();
         };
     }
 
-    bench_write!(KvStore);
-    bench_write!(Sled);
-    bench_read!(KvStore);
-    bench_read!(Sled);
+    let mut write_group = c.benchmark_group("write");
+    bench_write!(KvStore, write_group);
+    bench_write!(Sled, write_group);
+    write_group.finish();
+    let mut read_group = c.benchmark_group("read");
+    bench_read!(KvStore, read_group);
+    bench_read!(Sled, read_group);
+    read_group.finish();
 }
 
 criterion_group!(benches, benchmark);
